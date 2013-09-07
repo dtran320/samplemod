05b4182af20964b23a17594af8da98fa8078e230
David Tran <dtran320@gmail.com>
2013-09-07 00:27:00 -0700

    Add .gitnore, change author names, remove sphinx generated files

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.0.1',
-    description='Sample package for Python-Guide.org',
+    name='',
+    version='',
+    description='',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='David Tran',
+    author_email='david@crowdbooster.com',
+    url='',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
